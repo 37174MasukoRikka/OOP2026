81fcd3b81c65f09f3d09f1d9f75e5eccf13f4534
MusukoRikka <oibc37174@ojs.ac.jp>
2026-04-22 09:33:12 +0900

    コマンドライン引数の対応

--- Chapter02/距離換算プログラム/DistanceConventer/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConventer/Program.cs
@@ -1,31 +1,48 @@
-﻿namespace DistanceConventer
+﻿
+namespace DistanceConventer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             if (args.Length >= 1 && args[0] == "-tom")
-            {   //フィートからメートルへの対応表を出力
-                for (int feet = 1; feet <= 10; feet++)
-                {
-                    double meter = FeetToMeter(feet);
-                    Console.WriteLine($"{feet}ft = {meter:0.0000}m"); //$"{}
-                }
+            {
+                PrintFeetToMeterList(1, 10); //-tom メートルへの変換
+            }
+            else if (args.Length >= 1 && args[00] == "-tof")
+            {
+                PrintMeterToFeetList(1, 10); //-tof フィートへの変換
             }
             else
             {
-                //メートルからフィートへの対応表を出力
-                for (int meter = 1; meter <= 10; meter++)
-                {
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine($"{meter}m = {feet:0.0000}ft");
-                }
+                Console.WriteLine("引数エラー");
             }
         }
+
+        private static void PrintFeetToMeterList(int start, int stop)
+        {
+            //フィートからメートルへの対応表を出力
+            for (int feet = start; feet <= stop; feet++)
+            {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine($"{feet}ft = {meter:0.0000}m");  //$"{}
+            }
+
+        }
+        private static void PrintMeterToFeetList(int start, int stop)
+        {
+            //メートルからフィートへの対応表を出力
+            for (int meter = start; meter <= stop; meter++)
+            {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+            }
+        }
+
         //フィートからメートルを求める
         static double FeetToMeter(int feet)
         {
-           return feet * 0.3048;
+            return feet * 0.3048;
         }
 
         //メートルからフィートを求める
