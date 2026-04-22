1e74ec673bbf7c97ba55b6a7cf4ac180bf0639bb
MusukoRikka <oibc37174@ojs.ac.jp>
2026-04-22 11:46:30 +0900

    引数チェック

--- Chapter02/距離換算プログラム/DistanceConventer/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConventer/Program.cs
@@ -5,24 +5,32 @@ namespace DistanceConventer
     {
         static void Main(string[] args)
         {
-            if (args.Length >= 1 && args[0] == "-tom")
+
+            if (args.Length == 3 && int.TryParse(args[1], out int start) && int.TryParse(args[2], out int end))
             {
-                PrintFeetToMeterList(int.Parse(args[1]), int.Parse(args[2])); //-tom メートルへの変換
-            }
-            else if (args.Length >= 1 && args[00] == "-tof")
-            {
-                PrintMeterToFeetList(int.Parse(args[1]), int.Parse(args[2])); //-tof フィートへの変換
+
+                if (args[0] == "-tom")
+                {
+                    PrintFeetToMeterList(start, end); //-tom メートルへの変換
+                }
+                else if (args[0] == "-tof")
+                {
+                    PrintMeterToFeetList(start, end); //-tof フィートへの変換
+                }
+                else
+                {
+                    Console.WriteLine("引数形式エラー");
+                }
             }
             else
             {
                 Console.WriteLine("引数エラー");
             }
         }
-
-        private static void PrintFeetToMeterList(int start, int stop)
+        private static void PrintFeetToMeterList(int height, int stop)
         {
             //フィートからメートルへの対応表を出力
-            for (int feet = start; feet <= stop; feet++)
+            for (int feet = height; feet <= stop; feet++)
             {
                 double meter = FeetToMeter(feet);
                 Console.WriteLine($"{feet}ft = {meter:0.0000}m");  //$"{}
