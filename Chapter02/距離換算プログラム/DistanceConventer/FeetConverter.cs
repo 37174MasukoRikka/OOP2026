a96974b0b4df1be9184330e214deee51fd2df385
MusukoRikka <oibc37174@ojs.ac.jp>
2026-04-23 09:58:55 +0900

    静的クラスに変更する

--- Chapter02/距離換算プログラム/DistanceConventer/FeetConverter.cs
+++ Chapter02/距離換算プログラム/DistanceConventer/FeetConverter.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace DistanceConventer
 {
-    public class FeetConverter
+    public static class FeetConverter
     {
         //メートルからフィートを求める
-        public double FromMeter(double meter)
+        public static double FromMeter(double meter)
         {
             return meter * 0.3048;
         }
 
         //フィートからメートルを求める
-        public double ToMeter(double feet)
+        public static double ToMeter(double feet)
         {
             return feet * 0.3048;
         }
--- Chapter02/距離換算プログラム/DistanceConventer/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConventer/Program.cs
@@ -28,24 +28,20 @@ namespace DistanceConventer
         }
         //フィートからメートルへの対応表を出力
         private static void PrintFeetToMeterList(int start, int end)
-        {
-            FeetConverter converter = new FeetConverter();
-
+        {           
             for (int feet = start; feet <= end; feet++)
             {
-                double meter = converter.ToMeter(feet);
+                double meter = FeetConverter.ToMeter(feet);
                 Console.WriteLine($"{feet}ft = {meter:0.0000}m");  //$"{}
             }
 
         }
+        //メートルからフィートへの対応表を出力
         private static void PrintMeterToFeetList(int start, int end)
-        {
-            //メートルからフィートへの対応表を出力
-            FeetConverter converter = new FeetConverter();
-
+        {           
             for (int meter = start; meter <= end; meter++)
             {
-                double feet = converter.FromMeter(meter);
+                double feet = FeetConverter.FromMeter(meter);
                 Console.WriteLine($"{meter}m = {feet:0.0000}ft");
             }
         }
