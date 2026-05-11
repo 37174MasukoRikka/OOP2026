8da19870e4f73f3f17fa88fc4b1c322ce1dbcbf3
MasukoRikka <oibc37174@ojs.ac.jp>
2026-05-11 11:32:01 +0900

    2.1.4 完成

---
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            // 2.1.3
+            var songs = new Song[] {
+    new Song("Let it be", "The Beatles", 243),
+    new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+    new Song("Close To You", "Carpenters", 276),
+    new Song("Honesty", "Billy Joel", 231),
+    new Song("I Will Always Love You", "Whitney Houston", 273),
+};
+            PrintSongs(songs);　//←クリックして、Alt + Enter で以下のメソッドが自動的に作成される
+        }
+
+        private static void PrintSongs(Song[] songs) {
+            foreach (var song in songs) {
+                Console.WriteLine($"{song.Title},{song.ArtistName},{song.Length / 60 + ":" + (song.Length % 60).ToString("00")}");
+            }
+        }
+    }
+}
+
---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class Song {
+        //プロパティ
+        public string Title { get; set; } = string.Empty;
+        public string ArtistName { get; set; } = string.Empty;
+        public int Length { get; set; }
+
+        //コンストラクター
+        public Song(string title, string artistname, int length) {
+            Title = title;
+            ArtistName = artistname;
+            Length = length;
+        }
+    }
+}
