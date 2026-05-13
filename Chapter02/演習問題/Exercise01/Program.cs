2c3121d97c5ca7c9f7c1b3ecc6e7392789785754
MasukoRikka <oibc37174@ojs.ac.jp>
2026-05-13 10:20:51 +0900

    問題2.1 完成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -3,19 +3,41 @@ namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
             // 2.1.3
-            var songs = new Song[] {
-    new Song("Let it be", "The Beatles", 243),
-    new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
-    new Song("Close To You", "Carpenters", 276),
-    new Song("Honesty", "Billy Joel", 231),
-    new Song("I Will Always Love You", "Whitney Houston", 273),
-};
-            PrintSongs(songs);　//←クリックして、Alt + Enter で以下のメソッドが自動的に作成される
-        }
+            //var songs = new Song[] {
+            //new Song("Let it be", "The Beatles", 243),
+            //new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+            //new Song("Close To You", "Carpenters", 276),
+            //new Song("Honesty", "Billy Joel", 231),
+            //new Song("I Will Always Love You", "Whitney Houston", 273),
+            //};
 
-        private static void PrintSongs(Song[] songs) {
+            var songs = new List<Song>();
+            Console.WriteLine("***** 曲の登録  *****");
+
+            while (true) {
+                Console.Write("曲名：");
+                string? title = Console.ReadLine();
+                if (title == "end" || title == "End") {
+                    break;
+                }
+                Console.Write("アーティスト名：");
+                string? artistname = Console.ReadLine();
+                Console.Write("演奏時間（秒）：");
+                int length = int.Parse(Console.ReadLine());
+                //int length = Console.Read();
+                //Console.ReadLine(); //バッファクリア
+                Song song = new Song(title, artistname, length);
+
+                songs.Add(song);
+            }
+            PrintSongs(songs);
+            //PrintSongs(songs); //←クリックして、Alt + Enter で以下のメソッドが自動的に作成される
+        }
+        private static void PrintSongs(IEnumerable<Song> songs) {
             foreach (var song in songs) {
-                Console.WriteLine($"{song.Title},{song.ArtistName},{song.Length / 60 + ":" + (song.Length % 60).ToString("00")}");
+                var minutes = song.Length / 60;
+                var seconds = song.Length % 60;
+                Console.WriteLine($"{song.Title},{song.ArtistName},{minutes}:{seconds:00}");
             }
         }
     }
