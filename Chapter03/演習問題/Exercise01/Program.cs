d0d5084952c7883f2d70ce9825caa0fc50b87089
MasukoRikka <oibc37174@ojs.ac.jp>
2026-05-21 09:33:09 +0900

    問題3.1.2完成

--- Chapter03/演習問題/Exercise01/Program.cs
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -33,7 +33,7 @@ namespace Exercise01 {
         }
 
         private static void Exercise2(List<int> numbers) {
-
+            numbers.ForEach(n => Console.WriteLine(n / 2.0));
         }
 
         private static void Exercise3(List<int> numbers) {
