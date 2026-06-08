9a7f78478babf6209dd9fca8d4298af1d6802383
MasukoRikka <oibc37174@ojs.ac.jp>
2026-06-08 11:18:11 +0900

    5章終了

--- Chapter05/演習問題/Exercise01/Program.cs
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -2,8 +2,7 @@
     internal class Program {
         static void Main(string[] args) {
             var obj = new YearMonth(2020, 5);
-            Console.WriteLine(obj);
-            
+            Console.WriteLine(obj);           
         }
     }
 }
--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -1,7 +1,14 @@
-﻿namespace Exercise03 {
+﻿using Exercise01;
+
+namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            var p1 = new YearMonth(2002, 10);
+            var p2 = new YearMonth(2002, 10);
+            if (p1 == p2)
+                Console.WriteLine("等しい");
+            else
+                Console.WriteLine("等しくない");
         }
     }
 }
