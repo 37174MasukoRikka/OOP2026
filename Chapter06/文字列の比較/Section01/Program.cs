ae70aa43bd07f6695515faa06b133417ed787ee5
MasukoRikka <oibc37174@ojs.ac.jp>
2026-06-10 10:52:19 +0900

    問題6.2終了

---
+++ Chapter06/文字列の比較/Section01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var str1 = "あいうえお";
+            var str2 = "ｱｲｳｴｵ";
+
+            var cultureInfo = new CultureInfo("ja-JP");
+
+            if (String.Compare(str1, str2, cultureInfo,
+                             CompareOptions.IgnoreWidth | CompareOptions.IgnoreKanaType) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
---
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("整数文字列：");
+            var str = Console.ReadLine();
+
+            if (int.TryParse(str, out var num)) {
+                Console.WriteLine($"{num:#,0}");
+            } else {
+                Console.WriteLine("変換できません");
+            }
+        }
+    }
+}
