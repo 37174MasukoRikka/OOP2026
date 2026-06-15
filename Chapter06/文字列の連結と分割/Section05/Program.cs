d127096c18d1c0421e4220c22b0dcc52faae7845
MasukoRikka <oibc37174@ojs.ac.jp>
2026-06-15 10:36:22 +0900

    P145

---
+++ Chapter06/文字列の連結と分割/Section05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+            //var language = GetWords();
+            //var separator = ",";
+            var result = String.Join(",", GetWords());
+            Console.WriteLine(result);
+            //var sb = new StringBuilder();
+            //foreach (var word in GetWords()) {
+            //sb.Append(word);
+            //}
+        }
+        private static IEnumerable<string> GetWords() {
+            return ["Orange", "Lemon", "Strawberry"];
+        }
+    }
+}
