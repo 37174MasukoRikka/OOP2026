c7476681be1ef23fa3896ae523151d0080f8c798
MasukoRikka <oibc37174@ojs.ac.jp>
2026-06-22 11:33:59 +0900

    設問１_Studentクラス完成

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -1,7 +1,11 @@
 ﻿namespace Test01_01 {
     public class Student {
-        
-        
+        public required string Name { get; init; }
+        //学生の名前
+        public required string Subject { get; init; }
+        //科目名
+        public required int Score { get; init; }
+        //点数
 
     }
 }
