19b1db27b071bcd64592b9e2bbe2178f22b45712
MasukoRikka <oibc37174@ojs.ac.jp>
2026-06-25 15:38:13 +0900

    演習問題完成

---
+++ Chapter08/Section01/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Section01 {
+    internal class Program {
+        static private Dictionary<string, string> prefOfficeDict = new Dictionary<string, string>();
+
+
+        static void Main(string[] args) {
+            string? pref, prefCaptalLocation;
+
+            Console.WriteLine("県庁所在地の登録【入力終了：Ctrl + 'Z'】");
+
+            while (true) {
+                //都道府県入力
+                Console.Write("都道府県:");
+                pref = Console.ReadLine();
+
+                if (pref == null) break;   //無限ループを抜ける(Ctrl + Z)
+
+                //県庁所在地入力
+                Console.Write("県庁所在地:");
+                prefCaptalLocation = Console.ReadLine();
+
+                //県庁所在地登録処理
+                prefOfficeDict.Add(pref, prefCaptalLocation);
+
+            }
+            Console.WriteLine("***メニュー***\n1:一覧表示\n2:検索\n9:終了");
+            var line = Console.ReadLine();
+            int num = int.Parse(line);
+            switch (num) {
+                case 1:
+                    foreach (var item in prefOfficeDict) {
+                        Console.WriteLine($"{item.Key}の県庁所在地は{item.Value}です。");
+                    }
+                    break;
+                case 2:
+                    Console.Write("都道府県");
+                    var key = Console.ReadLine();
+                    if (prefOfficeDict.ContainsKey(key)) {
+                        var location = prefOfficeDict[key];
+                        Console.WriteLine($"{key}の県庁所在地は{location}です");
+                    }
+                    break;
+                case 9:
+                    break;
+            }
+        }
+    }
+}
