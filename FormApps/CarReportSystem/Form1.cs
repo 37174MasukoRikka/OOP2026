20f18e5252117470876793fbceb1113da01bcecb
MasukoRikka <oibc37174@ojs.ac.jp>
2026-08-25 14:17:16 +0900

    設定ファイル読み込み時のnull対応

--- FormApps/CarReportSystem/Form1.cs
+++ FormApps/CarReportSystem/Form1.cs
@@ -28,9 +28,12 @@ namespace CarReportSystem {
                 try {
                     using (var reader = XmlReader.Create("setting.xml")) {
                         var serializer = new XmlSerializer(typeof(Settings));
-                        settings = serializer.Deserialize(reader) as Settings;
-                        //îwåiêFê›íË
-                        BackColor = Color.FromArgb(settings.MainFormBackColor);
+
+                        if (serializer.Deserialize(reader) is Settings loadedSettings) {
+                            settings = loadedSettings;
+                            //îwåiêFê›íË
+                            BackColor = Color.FromArgb(settings.MainFormBackColor);
+                        }                      
                     }
                 }
                 catch (Exception ex) {
