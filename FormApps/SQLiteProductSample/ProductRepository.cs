446143e1bc8a922233e453042b92669c8b1b544f
MasukoRikka <oibc37174@ojs.ac.jp>
2026-08-31 15:31:02 +0900

    SELECT文の実装

--- FormApps/SQLiteProductSample/ProductRepository.cs
+++ FormApps/SQLiteProductSample/ProductRepository.cs
@@ -2,7 +2,36 @@ using Microsoft.Data.Sqlite;
 
 namespace SQLiteProductSample;
 
-public class ProductRepository
-{
-    
+//Productsテーブルに対するDB操作をまとめたクラス
+//CRUD (Create / Read / Update / Delete)を担当する
+public class ProductRepository {
+    public List<Product> GetAll() {
+
+        var products = new List<Product>();
+
+        using var connection = Database.GetConnection();
+        connection.Open();
+
+        //SQLを実行するためのコマンドオブジェクトを作る
+        using var command = connection.CreateCommand();
+
+        //Productsテーブルを作るSQL
+        command.CommandText =
+            """
+            SELECT Id, Name, Price
+            FROM Products
+            ORDER BY Id;
+            """;
+        //結果業を返さないSQLを実行する
+        using var reader = command.ExecuteReader();
+
+        while (reader.Read()) {
+            products.Add(new Product {
+                Id = reader.GetInt32(0),    //0列目:Id
+                Name = reader.GetString(1), //1列目:Name
+                Price = reader.GetInt32(2)  //2列目:Price
+            });
+        }
+        return products;
+    }
 }
